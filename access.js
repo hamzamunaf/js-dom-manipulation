406f49abcab5769a3e502fbf0eb9b223c09572a1
Rob Hess <robwhess@gmail.com>
2019-10-31 10:19:36 -0700

    Add code from 10/30 lecture.

--- access.js
+++ access.js
@@ -19,3 +19,18 @@ console.log("== photoCards:", photoCards);
 for (var i = 0; i < photoCards.length; i++) {
   console.log("  - photoCards[i]:", photoCards[i]);
 }
+
+// var images = document.getElementsByTagName('img');
+// console.log("== images:", images);
+//
+var firstPhotoCardImage = document.querySelector('.photo-card:first-child img');
+console.log("== firstPhotoCardImage:", firstPhotoCardImage);
+//
+// var photoCardContainer2 = document.querySelector('#photo-card-container');
+// console.log("== photoCardContainer2:", photoCardContainer2);
+//
+// var photoCards2 = document.querySelectorAll('.photo-card');
+
+var photoCard = photoCards[0];
+console.log("== photoCard.textContent:", photoCard.textContent);
+console.log("== photoCard.innerHTML:", photoCard.innerHTML);
---
+++ manipulate.js
@@ -0,0 +1,8 @@
+var photoCardContainer = document.getElementById('photo-card-container');
+
+function insertNewPhotoCard(url, caption) {
+  var photoCardSection = document.createElement('section');
+  console.log("== photoCardSection:", photoCardSection);
+}
+
+insertNewPhotoCard('http://placekitten.com/480/480?image=7', 'Luke as a kitty');
